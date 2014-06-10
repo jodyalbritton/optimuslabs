458ed044d31a13e8ee52b3355b2b02d9eb67a2e4
Jody Albritton <jody.albritton@gmail.com>
2014-06-10 14:32:46 -0500

    add sponsors

--- app/controllers/admin/sponsors_controller.rb
+++ app/controllers/admin/sponsors_controller.rb
@@ -10,6 +10,58 @@ class Admin::SponsorsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
+  # GET /sponsors/new
+  def new
+    @sponsor = Sponsor.new
+  end
+
+  # GET /sponsors/1/edit
+  def edit
+  end
+
+  # POST /sponsors
+  # POST /sponsors.json
+  def create
+    @sponsor = Sponsor.new(sponsor_params)
+
+    respond_to do |format|
+      if @sponsor.save
+        format.html { redirect_to ([:admin, @sponsor]), notice: 'Sponsor was successfully created.' }
+        format.json { render :show, status: :created, location: @sponsor }
+      else
+        format.html { render :new }
+        format.json { render json: @sponsor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /sponsors/1
+  # PATCH/PUT /sponsors/1.json
+  def update
+    respond_to do |format|
+      if @sponsor.update(sponsor_params)
+        format.html { redirect_to ([:admin, @sponsor]), notice: 'Sponsor was successfully updated.' }
+        format.json { render :show, status: :ok, location: @sponsor }
+      else
+        format.html { render :edit }
+        format.json { render json: @sponsor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sponsors/1
+  # DELETE /sponsors/1.json
+  def destroy
+    @sponsor.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_sponsors_url, notice: 'Sponsor was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 
    private
     # Use callbacks to share common setup or constraints between actions.
@@ -18,7 +70,7 @@ class Admin::SponsorsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def setting_params
+    def sponsor_params
       params.require(:sponsor).permit(:name, :description, :enabled, :category)
     end
 end
