dacf12be61b0b9971e1d4feaa70749ab84555ccd
Jody Albritton <jody.albritton@gmail.com>
2014-06-10 16:46:16 -0500

    fix images

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,5 +1,5 @@
 class Sponsor < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "600x403", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   belongs_to :category
 end
