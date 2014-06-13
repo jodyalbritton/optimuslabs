21777fcd383ad3e02ab053f35bebaa2ed9d95941
Jody Albritton <jody.albritton@gmail.com>
2014-06-13 12:58:39 -0500

    fix missing images

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ActiveRecord::Base
-	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png"
+	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
   	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
-  	has_attached_file :header, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing-header.jpg"
+  	has_attached_file :header, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing-header.jpg"
   	validates_attachment_content_type :header, :content_type => /\Aimage\/.*\Z/
 	belongs_to :category
 	has_many :items, :as => :purchasable
