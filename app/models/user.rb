928e05d9ba3bf3ee1f6b5cd7c46ea01289f885c8
Jody Albritton <jody.albritton@gmail.com>
2014-06-15 01:56:46 -0500

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,26 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  attr_accessor :login
 
  has_many :posts, :foreign_key => 'author_id'
  has_many :notes, :foreign_key => 'created_by_id'
  has_many :messages
  has_many :message_receipts, :as => :receivable
  
+ validates :username,
+  :uniqueness => {
+    :case_sensitive => false}
  
+ def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+end
+
  def full_name 
     first_name + " " + last_name
  end  
@@ -28,6 +40,14 @@ end
 def sent
 	message_receipts.sent
 end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
 
 
 end
