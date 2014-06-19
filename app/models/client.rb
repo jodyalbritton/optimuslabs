2477221cb794895dfc9f1ddd72159ef926adf165
Jody Albritton <jody.albritton@gmail.com>
2014-06-19 00:20:46 -0500

    fix photo

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :large => "600x600", :medium => "300x300#", :thumb => "100x100#" }, :default_url => ":style/missing.png"
-  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   	has_many :notes, as: :notable
   	has_many :invoices
   	has_many :tickets
