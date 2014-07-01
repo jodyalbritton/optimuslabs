3c19b60731698f5f17325b107359d928555e5c38
Jody Albritton <jody.albritton@gmail.com>
2014-07-01 06:57:43 -0500

    add google oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   config.omniauth :facebook, ENV["FB_KEY"], ENV["FB_SECRET"]
   config.omniauth :twitter, "KEY", "SECRET"
   config.omniauth :gplus, ENV["GPLUS_KEY"], ENV["GPLUS_SECRET"]
+  config.omniauth :google_oauth2, ENV["GPLUS_KEY"], ENV["GPLUS_SECRET"]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
