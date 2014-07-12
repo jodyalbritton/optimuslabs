babcf836f883f086e701a71178f18f075a88341d
Jody Albritton <jody.albritton@gmail.com>
2014-07-12 14:30:16 -0500

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,6 +105,7 @@ class User < ActiveRecord::Base
 
   def build_default_contact 
        build_contact
+       self.contact.email = self.email
        self.contact.first_name = self.first_name
        self.contact.last_name = self.last_name
        self.contact.user_id = self.id 
@@ -112,7 +113,7 @@ class User < ActiveRecord::Base
   end 
 
   def update_contact
-        
+        self.contact.email = self.email
         self.contact.first_name = self.first_name
         self.contact.last_name = self.last_name 
         self.contact.client_id = self.client_id
